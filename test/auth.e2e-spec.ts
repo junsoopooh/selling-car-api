a7c5ba509228366fc0836da87ad5e53139c5bc93
junsoopooh <junsoopooh@Naver.com>
2023-10-10 15:36:01 +0900

    section 14 update

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -18,12 +18,30 @@ describe('Authentication System', () => {
   it('handles a signup request', () => {
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({ email: 'jgjgjgjgjgjd@akl.com', password: 'qownstn' })
+      .send({ email: 'gksrmf@gksrmf.com', password: 'qo' })
       .expect(201)
       .then((res) => {
         const { id, email } = res.body;
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'wegweg@awegawegaeg.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'qo' })
+      .expect(201)
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email);
   });
 });
